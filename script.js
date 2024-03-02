ee34e27e9824a2d728750774364780fe24aebaba
Abhidadhich69 <144593987+Abhidadhich69@users.noreply.github.com>
2024-03-02 12:11:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ document.getElementById("page2-button-id").addEventListener('click', () => {
     window.scrollBy(0, window.innerHeight);   
 })
 
+const menuimgid=document.getElementById('menu-img-id');
+
+
+document.getElementById('m1').addEventListener('mouseenter',()=>{
+  menuimgid.src='Mastery.webp'
+})
+
+
+document.getElementById('m2').addEventListener('mouseenter',()=>{
+  menuimgid.src='ICONS.webp'
+})
+
+document.getElementById('m3').addEventListener('mouseenter',()=>{
+  menuimgid.src='Couture.webp'
+})
+
+document.getElementById('m4').addEventListener('mouseenter',()=>{
+  menuimgid.src='4-1.webp'
+})
+
+document.getElementById('m5').addEventListener('mouseenter',()=>{
+  menuimgid.src='Experiences_Menu_Image-1.webp'
+})
+
+
+const menuPage = document.getElementById("menu-bar-id")
+document.getElementById("nav-menu-id").addEventListener("click", ()=>{
+  menuPage.style.transform = "translatex(0%)"
+})
+
+document.getElementById("menu-close-btn").addEventListener("click", ()=>{
+  menuPage.style.transform = "translatex(100%)"
+})
